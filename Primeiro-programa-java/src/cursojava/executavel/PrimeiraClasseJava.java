e49147e5e186d1edba3bdbb8fae091de085abcf4
ayrton <ayrton@PC-ZOI>
2022-08-03 11:23:01 -0300

    Criação do Projeto

---
+++ Primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/*Main é um metódo executavel em Java*/
+	public static void main(String[] args) {
+	 
+		/*Escreve o texto no console*/
+		System.out.println("Meu primeiro programa em java");
+        System.out.println("Meu primeiro passo a FULL STACK");	
+	}
+	
+	
+}
